44305a832318399091c513e2947df1f762829a54
AshrithSathu <sathuashrith@gmail.com>
2024-02-16 23:31:07 +0530

    adding basic pizza images and basic ui

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -1,73 +1,63 @@
 const products = [
   {
     id: 1,
-    name: 'Ultimate Pepperoni',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: 12.99,
+    name: "Ultimate Pepperoni",
+    image: require("./images-pizza/peperoni.png"),
+    price: 1299,
   },
   {
     id: 2,
-    name: 'ExtravaganZZa',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 14.99,
+    name: "ExtravaganZZa",
+    image: require("./images-pizza/extravaganzza.png"),
+    price: 1499,
   },
   {
     id: 3,
-    name: 'MeatZZa',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: 13.47,
+    name: "MeatZZa",
+    image: require("./images-pizza/peperoni.png"),
+    price: 1347,
   },
   {
     id: 4,
-    name: 'Margarita',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
-    price: 9.9,
+    name: "Margarita",
+    image: require("./images-pizza/margarita.png"),
+    price: 990,
   },
   {
     id: 5,
-    name: 'Pacific Veggie',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.99,
+    name: "Pacific Veggie",
+    image: require("./images-pizza/veggie.png"),
+    price: 1299,
   },
   {
     id: 6,
-    name: 'Hawaiian',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
-    price: 10.49,
+    name: "Hawaiian",
+    image: require("./images-pizza/hawaiin.png"),
+    price: 1049,
   },
   {
     id: 7,
-    name: 'Deluxe',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
-    price: 16.99,
+    name: "Deluxe",
+    image: require("./images-pizza/deluxe.png"),
+    price: 1699,
   },
   {
     id: 8,
-    name: 'BBQ Chicken',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.89,
+    name: "BBQ Chicken",
+    image: require("./images-pizza/veggie.png"),
+    price: 1289,
   },
   {
     id: 9,
-    name: 'Chicken Bacon Ranch',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 13.99,
+    name: "Chicken Bacon Ranch",
+    image: require("./images-pizza/extravaganzza.png"),
+    price: 1399,
   },
   {
     id: 10,
-    name: '6 Cheese',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
-    price: 13.29,
+    name: "6 Cheese",
+    image: require("./images-pizza/6cheese.png"),
+    price: 1329,
   },
 ];
 
